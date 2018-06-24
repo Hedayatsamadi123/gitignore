d4828d53c1affde5177182f430bbdeed1fe93f26
Hedayatsamadi123 <Hedayatsamadi123@gmail.com>
2018-06-24 13:25:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// npm init -y To create package.json file
+// npm install isomorphic-fetch
+// npm install isomorphic-fetch --save
+// npm install mocha --save-dev
+
+
+const jokes = require('knock-knock-jokes');
+const haha = jokes();
+console.log(haha);
